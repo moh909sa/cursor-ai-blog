a5c8244ad06677b49f889b23d12f2162107ab90b
moh909sa <moh.saeed.shbnan997@gmail.com>
2025-06-26 20:57:50 +0300

    fix path

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the article-generator-cursor directory
 app.use(express.static(path.join(__dirname, 'article-generator-cursor')));
+
+// Ensure root path serves index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'article-generator-cursor', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
